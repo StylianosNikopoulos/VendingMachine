d01ac5bdc6d74f2c790b5fe01538e52cbc618b61
StylianosNikopoulos <nikstel2001@gmail.com>
2024-09-25 18:58:41 +0300

    UnitTests on deposit and buy
    Add unit tests for deposit and buy functionality in UserController

--- tests/Unit/UserControllerUnitTest.php
+++ tests/Unit/UserControllerUnitTest.php
@@ -12,7 +12,6 @@ use App\Models\User;
 class UserControllerUnitTest extends TestCase
 {
     use RefreshDatabase;
-
     protected $controller;
 
     //Parent function so other functions can inherit from it
@@ -20,16 +19,40 @@ class UserControllerUnitTest extends TestCase
         parent::setUp();
         $this->controller = new UserController(); 
     }
-    //Test calculateChange function to check if calculation is correct
+
+    //Test calculateChange function if calculation is correct
     /** @test */
-    public function it_calculates_change_correctly(){
+    public function testCalculateChange(){
         // Test various amounts
         $this->assertEquals([100, 50], $this->controller->calculateChange(150));
         $this->assertEquals([], $this->controller->calculateChange(0));
         $this->assertEquals([5], $this->controller->calculateChange(5));
         $this->assertEquals([100, 100, 5], $this->controller->calculateChange(205));
         $this->assertEquals([50, 5], $this->controller->calculateChange(55));
-    
     }
 
+    //Test deposit function if unauthenticated user can deposit cents
+    public function test_unauthenticated_user_cannot_deposit(){
+        $response = $this->post('/deposit', ['amount' => 10]);
+
+        // Check that the unauthenticated user is redirected to the login page
+        $response->assertStatus(302);  
+        $response->assertRedirect('/login'); 
+    }
+    
+    //Test buy function if unauthenticated user is can buy product
+    public function test_unauthenticated_user_cannot_buy(){
+        $product = Product::factory()->create([
+            'cost' => 100, 
+            'amountAvailable' => 10,
+            'productName' => 'A Product'
+        ]);
+    
+        $response = $this->post('/buy', [
+            'product_id' => $product->id,
+            'amount' => 1, 
+        ]);
+    
+        $response->assertStatus(401);
+    }
 }
