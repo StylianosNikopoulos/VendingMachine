242eca5137be1fd6f8cd1bacc03549c3ec44060c
Stylianos Nikopoulos <nikstel2001@gmail.com>
2024-09-28 12:56:25 +0300

    Update UserFactory.php

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -34,7 +34,7 @@ class UserFactory extends Factory
             'two_factor_secret' => null,
             'two_factor_recovery_codes' => null,
             'remember_token' => Str::random(10),
-            'role' => 'buyer', 
+            'role' => 'seller', 
             'profile_photo_path' => null,
             'current_team_id' => null,
         ];
